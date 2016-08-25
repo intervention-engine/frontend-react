f05550f84789a794885299377767dd1833ff9ef9
Noranda Brown <noranda@norandabrown.com>
2016-08-25 11:24:44 -0400

    Revert "Added and set up react-storybook "

--- src/components/Footer.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import Footer from './Footer';
-
-storiesOf('Footer', module)
-  .add('Basic Footer View', () => (
-    <Footer />
-  ));
