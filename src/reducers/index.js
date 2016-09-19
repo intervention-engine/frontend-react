af85aef21c5bc6d79fd070a21fb0b9394d1cb2f4
Andrew Schreiber <schreiaj@gmail.com>
2016-09-19 10:27:03 -0400

    Lint and Test Passes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import { routeReducer } from 'react-router-redux';
 import populationReducer from './population';
 import huddleReducer from './huddle';
 import riskAssessmentReducer from './risk_assessment';
-import PatientListResultsReducer from './PatientListResultsReducer'
+import PatientListResultsReducer from './PatientListResultsReducer';
 
 const rootReducer = combineReducers({
   routing: routeReducer,
