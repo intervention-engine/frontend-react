4584d5e28a3c5810d7183d784efa4a69ecdf5103
Andrew Schreiber <schreiaj@gmail.com>
2016-11-07 11:11:48 -0500

    fix linter errors

--- src/containers/Patient.js
+++ src/containers/Patient.js
@@ -10,47 +10,47 @@ import PatientView from '../components/PatientView/PatientView';
 
 class Patient extends Component {
 
-	componentWillMount(){
+  componentWillMount(){
 
-		this.props.selectPatient(this.props.params.patient_id);
-		if(!(this.props.params.patient_id == this.props.selectedPatient.id)) {
+    this.props.selectPatient(this.props.params.patient_id);
+    if(!(this.props.params.patient_id == this.props.selectedPatient.id)) {
 			// Fire action to set the selectedPatient correctly
-			this.props.fetchPatient(this.props.params.patient_id);
-		}
-	}
+      this.props.fetchPatient(this.props.params.patient_id);
+    }
+  }
 
-	render() {
-		return (
+  render() {
+    return (
 			<div className='patient'>
 				<PageHeader title="Patients"/>
 				<PatientView patient={this.props.selectedPatient} />
 			</div>
 		);
-	}
+  }
 }
 
 Patient.contextTypes = {
-	router: PropTypes.object
+  router: PropTypes.object
 };
 
 Patient.propTypes = {
-	selectedPatient: patientProps,
-	selectPatient: PropTypes.func.isRequired,
-	fetchPatient: PropTypes.func.isRequired,
-	params: PropTypes.shape({patient_id: PropTypes.string}).isRequired
+  selectedPatient: patientProps,
+  selectPatient: PropTypes.func.isRequired,
+  fetchPatient: PropTypes.func.isRequired,
+  params: PropTypes.shape({patient_id: PropTypes.string}).isRequired
 };
 
 function mapStateToProps(state) {
-	return {
-		selectedPatient: state.patient.selectedPatient
-	};
+  return {
+    selectedPatient: state.patient.selectedPatient
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({
-		fetchPatient,
-		selectPatient
-	}, dispatch);
+  return bindActionCreators({
+    fetchPatient,
+    selectPatient
+  }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Patient);
