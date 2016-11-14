d70555d2267658fd714c9ad7ec59a8cf75bafb4a
Andrew Schreiber <schreiaj@gmail.com>
2016-11-14 14:59:11 -0500

    Added voronoi click targets

--- src/components/PatientView/PatientViewBanner/PatientViewBannerRiskChart.js
+++ src/components/PatientView/PatientViewBanner/PatientViewBannerRiskChart.js
@@ -2,28 +2,34 @@ import React from 'react';
 
 import { scaleTime, scaleLinear } from 'd3-scale';
 import { extent } from 'd3-array';
-import { area } from 'd3-shape';
+import { area, line } from 'd3-shape';
+import { voronoi } from 'd3-voronoi';
 
-const PatientViewBannerRiskChart = ({ riskAssessments, onClickHandler=function(index){}}) => {
+const PatientViewBannerRiskChart = ({ riskAssessments, onClickHandler=function(index){console.log(index);}}) => {
   riskAssessments = riskAssessments.sort((a,b) => new Date(a.datetime) - new Date(b.datetime));
   let width = 100.0/riskAssessments.length
   let timeScale = scaleTime()
     .domain(extent(riskAssessments, (r) => new Date(r.datetime)))
     .range([0,100]);
   let riskScale = scaleLinear().domain([4,0]).range([1,19]);
-  let areaGenerator = area()
-    .x((d) => timeScale(new Date(d.datetime)))
-    .y1((d) => riskScale(d.value))
-    .y0(100);
+  let areaGenerator = area().y0(100);
+  // Define a function to project the riskAssessments into the svg's coordinate system
+  // This will make building diagram easier
+  // Note ,if you need more data from the riskAssessments the indexes line up
+  let projection = (d) => [timeScale(new Date(d.datetime)), riskScale(d.value)];
+  let hitTargets = voronoi().extent([[-1, -1], [101, 21]]);
+
   if (riskAssessments.length > 0) {
+    let projectedData = riskAssessments.map(projection);
+    let hitTargetPolys = hitTargets.polygons(projectedData);
     return (
       <div>
         <svg viewBox='0 0 100 20' width='100%' height='100%'>
-          <path d={areaGenerator(riskAssessments)} />
-          {riskAssessments.map((ra, i) =>
-            <g onClick={() => onClickHandler(i)} key={i}>
-              <circle cx={timeScale(new Date(ra.datetime))} cy={riskScale(ra.value)} r={0.6} />
-              <rect y={0} height={20} x={timeScale(new Date(ra.datetime))} width={100-timeScale(new Date(ra.datetime))} opacity={0} stroke='black'/>
+          <path d={areaGenerator(projectedData)} className='path'/>
+          {projectedData.map((ra, i) =>
+            <g key={i}>
+              <circle cx={ra[0]} cy={ra[1]} r={0.6} className='data-point'/>
+              <path d={line()(hitTargetPolys[i])} opacity={0} onClick={() => onClickHandler(i)} className='click-target'/>
             </g>
           )}
         </svg>
