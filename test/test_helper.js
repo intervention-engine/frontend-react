38256f99cf8e87f0eada9d1f7d42fb84955d1ff2
Andrew Schreiber <schreiaj@gmail.com>
2017-05-24 11:03:38 -0400

    remove erroneous lib

--- test/test_helper.js
+++ test/test_helper.js
@@ -7,7 +7,6 @@ import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import check from 'react-prop-types-check';
 import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -36,14 +35,5 @@ $.fn.simulate = function(eventName, value) {
   TestUtils.Simulate[eventName](this[0]);
 };
 
-let isProp = function(obj, propType){
-  try {
-    check(obj, propType, true)
-  } catch (e) {
-    console.error(e.message);
-    return false;
-  }
-  return true;
-}
 
-export {renderComponent, expect, isProp};
+export {renderComponent, expect};
