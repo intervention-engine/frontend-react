e0b784b8e75ad20ca9cc16141b1f92bd666ba269
Andrew Schreiber <schreiaj@gmail.com>
2017-05-24 10:42:53 -0400

    Linter error and prop fixes to handle the lack of huddles

--- src/components/PatientList/PatientListResults/PatientListResultsItem.js
+++ src/components/PatientList/PatientListResults/PatientListResultsItem.js
@@ -16,7 +16,8 @@ import riskAssessmentTypeProps from '../../../prop-types/risk_assessment_type';
 
 export default class PatientListResultsItem extends Component {
   renderedNextHuddle(patient) {
-    return null;
+    if(!this.props.nextHuddles)
+      return null;
     let nextHuddle = this.props.nextHuddles[patient.id];
 
     if (nextHuddle == null) {
@@ -110,7 +111,7 @@ PatientListResultsItem.propTypes = {
   patient: patientProps.isRequired,
   huddles: PropTypes.arrayOf(huddleGroupProps).isRequired,
   filteredRiskAssessments: riskAssessmentProps,
-  nextHuddles: PropTypes.object.isRequired,
+  nextHuddles: PropTypes.object,
   selectedRiskAssessment: riskAssessmentTypeProps.isRequired,
 };
 
