d79aef79aaa26eb45b477c38e9f75946a7587958
Andrew Schreiber <schreiaj@gmail.com>
2017-06-19 12:23:17 -0400

    init dragula

--- src/containers/FilterBuilder.js
+++ src/containers/FilterBuilder.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import PageHeader from '../components/Header/PageHeader';
+import Dragula from 'react-dragula';
 
-const FilterBuilder = () => {
-  return (
-    <div className="filter-builder container">
-      <PageHeader title="Filter Builder" />
+class FilterBuilder extends Component {
 
-      <div>Filter Builder</div>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {selected: []};
+  }
+
+  dragController = null;
+
+  render() {
+    return (
+      <div className="filter-builder container">
+        <PageHeader title="Filter Builder" />
+        <div>Filter Builder</div>
+        <div className='container' ref={this.dragulaDecorator.bind(this)}>
+          <div className='source'>
+            <div style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap me around</div>
+            <div style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap her around</div>
+            <div style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap him around</div>
+            <div style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap them around</div>
+            <div style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap us around</div>
+            <div style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap things around</div>
+            <div data-filter-type='test' style={{margin: '5px', padding:'5px', border: '1px solid black'}}>Swap everything around</div>
+          </div>
+          <div className='target'>Test
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+
+  onFilterDrop(el) {
+    // debugger
+    this.setState({selected: [].concat(this.state.selected, el)})
+    console.log(this.state);
+    return true;
+  }
+
+  dragulaDecorator(component) {
+    if(component) {
+      let options = {
+
+      };
+      let dragController = Dragula([component.querySelector('.source'), component.querySelector('.target')], options);
+      dragController.on('drop', this.onFilterDrop.bind(this));
+    }
+  }
 };
 
 FilterBuilder.displayName = 'FilterBuilder';
